5d6cad783913a1c7b2758776432bc704f96d8886
平头哥 <oncwnuM3kr4gzkR8gi52AQSjKbTU@git.weixin.qq.com>
2019-12-10 20:56:30 +0800

    BeanUtils

--- src/main/java/com/freedom/wechat/toolspack/BeansUtils.java
+++ src/main/java/com/freedom/wechat/toolspack/BeansUtils.java
@@ -4,6 +4,7 @@ package com.freedom.wechat.toolspack;
 import org.apache.commons.beanutils.BeanUtils;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 /**
  * Created by freedom on 2019/12/4.
@@ -15,6 +16,22 @@ public class BeansUtils {
         BeanUtils.setProperty(students, "name","name");
         BeanUtils.setProperty(students, "password", "password1");
         System.out.println(students.getName() + "  " + students.getPassword() );
+
+        try {
+            Map<String,String > map = BeanUtils.describe(students);
+            for (String key:map.keySet()){
+                System.out.println("map   " + key + map.get(key));
+            }
+
+            map.put("name", "mapName");
+            BeanUtils.populate(students,map);
+            System.out.println("convert " + students.getName());
+
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
